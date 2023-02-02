fedad94fdc95f41b3851b3b3f065afea3ff6dd9e
Thodoris Greasidis <thgreasi@gmail.com>
2023-02-02 11:21:37 +0200

    types: Add type checks for the direction of the string $orderby variant
    Change-type: patch

--- typings/pinejs-client-core.d.ts
+++ typings/pinejs-client-core.d.ts
@@ -157,7 +157,7 @@ interface Lambda<T> {
 
 type OrderByDirection = 'asc' | 'desc';
 type OrderBy<T> =
-	| string // TODO next major: Change to: `${keyof T & string} ${OrderByDirection}` | [keyof T & string, OrderByDirection]
+	| `${string} ${OrderByDirection}` // TODO next major: Change to: `${keyof T & string} ${OrderByDirection}` | [keyof T & string, OrderByDirection]
 	| Array<OrderBy<T>>
 	| { [k in keyof T]?: OrderByDirection }
 	| ({
